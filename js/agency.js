a9c1dfb8fd85107ba86c46163fa282bf756ca4ce
moyersd28-cyber <moyersd28@gmail.com>
2026-07-07 21:46:40 -0400

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -1,1 +1,33 @@
+let agency = null;
 
+
+async function loadAgency(){
+
+    try {
+
+        const response = await fetch("../config/agency.json");
+
+        agency = await response.json();
+
+
+        console.log(
+            "Loaded Agency:",
+            agency.name
+        );
+
+
+    }
+
+    catch(error){
+
+        console.error(
+            "Agency loading failed:",
+            error
+        );
+
+    }
+
+}
+
+
+loadAgency();
